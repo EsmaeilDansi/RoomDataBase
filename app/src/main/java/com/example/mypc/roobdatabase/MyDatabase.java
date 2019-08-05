e88779ff2883647e32e8eccfc7f831d40618a4bc
dansigit <e.dansi.111@gmail.com>
2019-08-05 12:02:22 +0430

    new message

--- app/src/main/java/com/example/mypc/roobdatabase/MyDatabase.java
+++ app/src/main/java/com/example/mypc/roobdatabase/MyDatabase.java
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase;
 @Database(entities = {Information.class,Newclass.class,Myclass.class},version = 6)
 public abstract class MyDatabase extends RoomDatabase
 {
-    public abstract MyDao myDao();
+    //public abstract MyDao myDao();
     public abstract NewDao newDao();
     public abstract Dao2 dao2();
 
